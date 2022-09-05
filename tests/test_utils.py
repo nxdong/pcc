a4e7fa2038c2b1f3a418c53a98687b2d874ed10d
ningxiangdong <ningxiangdong@sensetime.com>
2022-09-05 11:57:46 +0800

    update deps

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -13,8 +13,10 @@ def test_charset_utf8():
     file_path = './tests/data/charset/utf8.py'
     with open(file_path, 'rb') as f:
         str_bytes = f.read()
-    content = uniform_charset(str_bytes)
-    content = content.decode('utf-8')
+    # content = uniform_charset(str_bytes)
+    # content = content.decode('utf-8')
+    content = from_bytes(str_bytes).best().output(
+        encoding='utf_8').decode('utf-8')
     assert content == file_content_ground_truth
 
 
@@ -22,20 +24,26 @@ def test_charset_utf8_bom():
     file_path = './tests/data/charset/utf8_bom.py'
     with open(file_path, 'rb') as f:
         str_bytes = f.read()
-    content = uniform_charset(str_bytes)
-    content = content.decode('utf-8')
+    # content = uniform_charset(str_bytes)
+    # content = content.decode('utf-8')
+    content = from_bytes(str_bytes).best().output(
+        encoding='utf_8').decode('utf-8')
     assert content == file_content_ground_truth
+
 
 def test_charset_file_utf8():
     file_path = './tests/data/charset/utf8.py'
-    content = uniform_charset_from_file(file_path)
-    content = content.decode('utf-8')
+    # content = uniform_charset_from_file(file_path)
+    # content = content.decode('utf-8')
+    content = from_path(file_path).best().output(
+        encoding='utf_8').decode('utf-8')
     assert content == file_content_ground_truth
 
 
 def test_charset_file_utf8_bom():
     file_path = './tests/data/charset/utf8_bom.py'
-    content = uniform_charset_from_file(file_path)
-    content = content.decode('utf-8')
+    # content = uniform_charset_from_file(file_path)
+    # content = content.decode('utf-8')
+    content = from_path(file_path).best().output(
+        encoding='utf_8').decode('utf-8')
     assert content == file_content_ground_truth
-
